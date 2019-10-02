5e74afc89b9b6185698b0f486b3f6b354fd59ded
João Zarco <1170531@isep.ipp.pt>
2019-10-02 11:54:56 +0100

    refactoring all class names

--- Project/Models/Domain/MasterDataFactory/ProductionLine/ProductionLineId.cs
+++ Project/Models/Domain/MasterDataFactory/ProductionLine/ProductionLineId.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Project.Models.Domain.MasterDataProduct.ProductionLine
+namespace Project.Models.Domain.MasterDataFactory.ProductionLine
 {
     public class ProductionLineId
     {
