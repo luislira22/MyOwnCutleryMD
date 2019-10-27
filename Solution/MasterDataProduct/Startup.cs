16731f547dad9322a42c7d04e6d774ffe9215350
Guilherme Oliveira <1170814@isep.ipp.pt>
2019-10-27 22:41:08 +0000

    MasterDataProduct Migrations

--- Solution/MasterDataProduct/Startup.cs
+++ Solution/MasterDataProduct/Startup.cs
@@ -24,7 +24,8 @@ namespace MasterDataProduct
             // Exemplo da ligação à BD
             //var connection = "Server=lapr2019.database.windows.net,1433;Database=lapr5;User ID=lapr;Password=YoHwGciYDXaUcjmt75J6;";
             //services.AddDbContext<Context>(opt => opt.UseSqlServer(connection));
-            services.AddDbContext<Context>(opt => opt.UseInMemoryDatabase("MDF"));
+            //services.AddDbContext<Context>(opt => opt.UseInMemoryDatabase("MDF"));
+            services.AddDbContext<Context>(opt => opt.UseSqlServer(Configuration.GetConnectionString("AzureDB")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             //services.BuildServiceProvider().GetService<Context>().Database.Migrate();
         }
