9c9600e5257dcb509d491fc27bcc58b6be32c82f
Tomás da Silva Soares <1170635@isep.ipp.pt>
2019-11-10 13:08:18 +0000

    Three js factory floor, machines and walls

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,27 +9,4 @@ function App(){
     )
   }
 
-  /*
-        <div className="content">
-          <Machine/>
-        </div>
-  */
-  //Before last commit
-  // render() {
-  //   return (
-  //     <div>
-  //       <h1>MyOwnCutlery</h1>
-  //       <hr/>
-  //       <div className="content">
-  //         <Machine/>
-  //       </div>
-
-  //       <div className="content">
-  //         <MachineTypes machineTypes={this.state.machineTypes} />
-  //       </div>
-  //     </div>
-
-  //   )
-  // }
-
 export default App;
