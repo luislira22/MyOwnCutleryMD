d056aac3d18f73d6a2a777ceed1058e24a997382
Guilherme Oliveira <1170814@isep.ipp.pt>
2019-11-15 16:49:05 +0000

    Cors policy applied

--- Solution/MasterDataProduct/Startup.cs
+++ Solution/MasterDataProduct/Startup.cs
@@ -1,11 +1,10 @@
-﻿using MasterDataProduct.Models;
-using MasterDataProduct.PersistenceContext;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using MasterDataProduct.PersistenceContext;
 
 namespace MasterDataProduct
 {
@@ -28,6 +27,12 @@ namespace MasterDataProduct
             //services.AddDbContext<Context>(opt => opt.UseSqlServer(Configuration.GetConnectionString("AzureDB")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             //services.BuildServiceProvider().GetService<Context>().Database.Migrate();
+            services.AddCors(o => o.AddPolicy("SPA", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,11 +47,7 @@ namespace MasterDataProduct
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseCors(builder => builder.WithOrigins("http://localhost:3000")
-                .AllowAnyMethod()
-                .AllowAnyHeader());
-
+            app.UseCors("SPA");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
