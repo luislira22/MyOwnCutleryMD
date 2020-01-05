3ba141d04d97ae3f6c007e000a0bce1b28fabf78
Guilherme Oliveira <1170814@isep.ipp.pt>
2020-01-05 15:54:17 +0000

    Authentication MDF [Fix] [refs #7]

--- Solution/MasterDataFactory/Controllers/FactoryOverviewController.cs
+++ Solution/MasterDataFactory/Controllers/FactoryOverviewController.cs
@@ -2,10 +2,12 @@ using System.Threading.Tasks;
 using MasterDataFactory.DTO.ProductionPlanning;
 using MasterDataFactory.Models.PersistenceContext;
 using MasterDataFactory.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MasterDataFactory.Controllers
 {
+    [AllowAnonymous]
     [Route("api/[controller]")]
     [ApiController]
     public class FactoryOverviewController : ControllerBase
